fb4d1371c1572e9c7e63296a95af54a28e90260c
mendokusaiiii <victor_cabral_o@hotmail.com>
2022-04-06 21:18:46 -0300

    COmeço dos exercicios do bloco 4

---
+++ fundamentos/bloco-4-introdução-js/scripts.js
@@ -0,0 +1,24 @@
+let a = 5;
+let b = 6;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a%b);
+
+const maior = 8;
+const menor = 7;
+
+if(maior > menor){
+    console.log("É maior");
+    }
+else {
+    console.log("Menor");
+    }
+
+
+const valor1 = 2;
+const valor2 = 9;
+const valor3 = 4;
+
+
